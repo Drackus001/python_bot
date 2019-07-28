685e0ade08b4dfc635dd1b31d03020441741bef5
root <root@localhost.localdomain>
2019-07-28 12:00:51 +0530

    hashtag search is implemented

--- app.py
+++ app.py
@@ -11,14 +11,20 @@ class TwitterBot:
     def login(self):
         bot = self.bot
         bot.get('https://twitter.com/')
-        time.sleep(3)
+        time.sleep(2)
         email = bot.find_element_by_class_name('email-input')
         password = bot.find_element_by_name('session[password]')
         email.clear()
         password.clear()
-        email.send_keys(self.username)
+        email.send_keys(self.username)  
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
+        time.sleep(2)
 
+    def like_tweet(self, hastag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q='+hastag+'&src=typed_query')
+        time.sleep(2)
 satyam = TwitterBot('username', 'password')
 satyam.login()
+satyam.like_tweet('webscrapling')
