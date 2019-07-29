aa0dcc02e24b68621a4002c74868d69cafd73fbb
root <root@localhost.localdomain>
2019-07-29 14:01:47 +0530

    autolinking is working without login

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class TwitterBot:
     def __init__(self, username, password):
@@ -16,6 +20,7 @@ class TwitterBot:
         time.sleep(2)
         email = bot.find_element_by_class_name('email-input')
         password = bot.find_element_by_name('session[password]')
+
         email.clear()
         password.clear()
         print('email:', email)
@@ -24,68 +29,82 @@ class TwitterBot:
         email.send_keys(self.username)
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
+
         print('sending request for login...')
         time.sleep(3)
 
     def like_tweet(self, hastag):
         bot = self.bot
+        time.sleep(3)
         bot.get('https://twitter.com/search?q='+hastag+'&src=typed_query')
         print('searching for ', hastag, '...')
 
-        
         time.sleep(3)
         scroll = 300
-        for i in range(1, 10):
-            time.sleep(5)
-            
+        for i in range(0, 5):
+            time.sleep(3)
+
             # bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
             with open('jquery.js', 'r') as jquery_js:
                 jquery = jquery_js.read()  # read the jquery from a file
                 bot.execute_script(jquery)  # active the jquery lib
                 print('Scrolling...')
-                script = "$(document).ready(function(){$('body, html').animate({scrollTop: "+str(scroll)+"}, 800)})"
+                script = "$(document).ready(function(){$('body, html').animate({scrollTop: "+str(
+                    scroll)+"}, 800)})"
                 bot.execute_script(script)
                 scroll += 500
-            #bot.execute_script(
-            #"$(document).ready(function(){$('body, html').animate({scrollTop: 156}, 800)})")
             
-            #tweets = bot.find_element_by_class_name('r-1udh08x')
-            #Xpath= "//div[@class='css-1dbjc4n r-1udh08x r-utggzx']"
-            # css-1dbjc4n r-18u37iz r-thb0q2
-            # tweets=bot.find_elements_by_class_name(
-            #    'css-1dbjc4n r-18u37iz r-thb0q2')
-            # print(tweets.__getattribute__('href'))
-            # tweets.send_keys(Keys.RETURN)
-            #tweets2 = bot.find_elements_by_xpath("//div[@class='css-1dbjc4n r-18u37iz r-thb0q2']")
-            # tweets.send
-            # new WebDriverWait(bot, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'css-1dbjc4n r-1udh08x r-utggzx')]"))).click();
-            #xyz = bot.find_elements_by_css_selector("div[class$='css-1dbjc4n r-1udh08x r-utggzx']")
-            # new WebDriverWait(bot, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class$='css-1dbjc4n r-1udh08x r-utggzx'][data-testid='tweet']"))).click();
-            # print(xyz.count)
-
-            # href="/TwistaTim/status/1155545324846403584
+            # Tweet
             # css-4rbku5 css-18t94o4 css-901oao r-1re7ezh r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0
-            # __getattribute__('href')
-            # links=[elem
+            # css-4rbku5 css-18t94o4 css-901oao r-1re7ezh r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0
+            # r-1q142lx
+            # Heart
+            # css-1dbjc4n r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-xf4iuw r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg
+            # r-xf4iuw
+
+            # css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr
+            # r-bztko3
+            
+            tweets = bot.find_elements_by_class_name('tweet')
+            #tweets = bot.find_elements_by_class_name()
+            #tweets = bot.find_elements_by_class_name('css-4rbku5 css-18t94o4 css-901oao r-1re7ezh r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0')
+            #tweets = bot.find_elements_by_xpath("//div[@class='css-4rbku5 css-18t94o4 css-901oao r-1re7ezh r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0']")
+            
+            #print(tweets)
+            links = [elem.get_attribute('data-permalink-path')
+                     for elem in tweets]
+            # links = [elem.get_attribute('href')
             #         for elem in tweets]
-            # print(links)
-            # for x in xyz:
-            #     print(x.value)
-            #     print(x.sep)
+            print(links)
+            for link in links:
+                bot.get('https://twitter.com'+link)
+                print(link + 'visiting...')
+            #     if(link != None):
+            #         bot.get(link)
+            #         print('link visited')
+                try:
+                    bot.find_element_by_class_name('HeartAnimation').click()
+            #             clickable = bot.find_elements_by_class_name('r-bztko3')
+            #             arr_clickable = [elem.get_attribute('data-testid')
+            #                         for elem in clickable]
+            #             for x in arr_clickable:
+            #                 if(x == 'like'):
+            #                     print('liked...')
+            #                     #bot.find_element_by_class_name('css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr').click()
+                    time.sleep(10)
+                except Exception as ex:
+                    time.sleep(60)
+            
+            
+                      
 
-            #xxx = bot.find_elements_by_class_name("div[class$='css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr'][data-testid='like']")
-
-            # time.sleep(2)
-            # for link in links:
-            #    bot.get('https://twitter.com' + link)
-            #    try:
-            #        bot.find_element_by_class_name('css-ldbjc4n').click()
-            #        time.sleep(7)
-            #    except Exception as ex:
-            #        time.sleep(60)
+            
 
 
-satyam = TwitterBot('username', 'password')
-satyam.login()
-# satyam.like_tweet('automation')
-satyam.like_tweet('helloworld')
+user = TwitterBot('username', 'password')
+#user.login()
+user.like_tweet('automation')
+#user.like_tweet('helloworld')
+
+
+# css-1dbjc4n r-1loqt21 r-1udh08x r-utggzx r-1wqkr8a
